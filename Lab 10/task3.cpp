ba5ae163c29955b1d2a5444da214ed4acd5036df
TheProminentAhmed <ahmedzeb66.az@gmail.com>
2025-11-16 23:26:42 +0500

    Update task3.cpp

--- Lab 10/task3.cpp
+++ Lab 10/task3.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int t=a; a=b; b=t;
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+
+    if (l < n && arr[l] > arr[largest]) largest = l;
+    if (r < n && arr[r] > arr[largest]) largest = r;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+int kthLargest(int arr[], int n, int k) {
+    for (int i = n/2 - 1; i >= 0; i--) heapify(arr, n, i);
+
+    for (int i = n-1; i >= n-k+1; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+    return arr[0];
+}
+
+int main() {
+    int arr[7] = {1,23,12,9,30,2,50};
+    int k = 3;
+    cout << kthLargest(arr, 7, k);
+}
 
